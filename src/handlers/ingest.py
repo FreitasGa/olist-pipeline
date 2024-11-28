060ef545a0b07e6ec0b5c7634a8314e045b0e09c
Gabriel Freitas <freitasga.01@gmail.com>
2024-11-28 20:45:17 -0300

    update ingest

--- src/handlers/ingest.py
+++ src/handlers/ingest.py
@@ -14,18 +14,11 @@ logger.setLevel(logging.INFO)
 def run(event, context):
     logger.info(f"Ingesting dataset, at {datetime.datetime.now()}")
 
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_REGION = os.getenv("AWS_REGION")
     KAGGLE_USERNAME = os.getenv("KAGGLE_USERNAME")
     KAGGLE_KEY = os.getenv("KAGGLE_KEY")
     DATASET_URL = os.getenv("DATASET_URL")
     DATASET_DIR = os.getenv("DATASET_DIR")
-    AWS_S3_BUCKET_NAME = os.getenv("AWS_S3_BUCKET_NAME")
-
-    if not AWS_ACCESS_KEY_ID or not AWS_SECRET_ACCESS_KEY or not AWS_REGION:
-        logger.error("AWS credentials not found")
-        raise
+    BUCKET_NAME = os.getenv("BUCKET_NAME")
 
     if not KAGGLE_USERNAME or not KAGGLE_KEY:
         logger.error("Kaggle credentials not found")
@@ -35,7 +28,7 @@ def run(event, context):
         logger.error("Dataset information not found")
         raise
 
-    if not AWS_S3_BUCKET_NAME:
+    if not BUCKET_NAME:
         logger.error("S3 bucket name not found")
         raise
 
@@ -64,19 +57,14 @@ def run(event, context):
             for file in files:
                 paths.append(os.path.join(root, file))
 
-        client = boto3.client(
-            "s3",
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-            region_name=AWS_REGION
-        )
+        client = boto3.client("s3")
 
         logger.info("Uploading files to S3")
 
         for path in paths:
             client.upload_file(
                 path,
-                AWS_S3_BUCKET_NAME,
+                BUCKET_NAME,
                 os.path.basename(path)
             )
 
